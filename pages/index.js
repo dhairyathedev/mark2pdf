714bf561536654a816191ce477275fa6c9f4e7fc
SnowBit <dtshah24022006@gmail.com>
2022-04-21 11:56:53 +0530

    Changed urls and updated the title

--- pages/index.js
+++ pages/index.js
@@ -64,13 +64,13 @@ export default function New() {
     <link rel="manifest" href="/site.webmanifest" />
     </Head>
         <NextSeo
-            title="Convert your markdown to beautiful PDF in seconds with our online converter - mark2pdf"
+            title="Convert your markdown to beautiful PDF in seconds - mark2pdf"
             description="Paste your markdown markup and we will convert it to a PDF file"
-            canonical="http://www.mark2pdf.vercel.app/"
+            canonical="http://mark2pdf.vercel.app/"
             openGraph={{
-                title: 'Convert your markdown to beautiful PDF with our online converter - mark2pdf',
+                title: 'Convert your markdown to beautiful PDF in seconds - mark2pdf',
                 description: 'Paste your markdown markup and we will convert it to a PDF file',
-                url: 'https://www.mark2pdf.vercel.app/',
+                url: 'https://mark2pdf.vercel.app/',
                 site_name: 'mark2pdf',
             }}
             twitter={{
