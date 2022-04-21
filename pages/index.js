1bdb0bb1419341859bd27c636ac7fc47b47c6d93
SnowBit <dtshah24022006@gmail.com>
2022-04-21 16:26:25 +0530

    Changed placeholder

--- pages/index.js
+++ pages/index.js
@@ -99,7 +99,7 @@ export default function New() {
         <div className="flex flex-row items-center space-x-4">
           <a href="https://twitter.com/codewithsnowbit" target="_blank" rel="noopener noreferrer" >
           <div className="px-3 py-[7px] bg-white item border border-[#ccc] rounded-md hidden sm:flex items-center justify-center hover:shadow-md">
-          <Image src="https://avatars.githubusercontent.com/codewithsnowbit?s=30" alt="Dhairya Shah" class="rounded-full border-2 border-[#1DA1F2] p-[2px]" width={24} height={24}/>
+          <Image src="https://avatars.githubusercontent.com/codewithsnowbit?s=30" alt="Dhairya Shah" className="rounded-full border-2 border-[#1DA1F2] p-[2px]" width={24} height={24}/>
           <div className="ml-2 flex items-start justify-center flex-col w-full"><h3 className="font-bold capitalize text-sm">By Dhairya Shah</h3><p className="text-[10px] font-medium flex items-center text-[#1DA1F2]">Follow on Twitter<svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" className="ml-1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg></p></div>
 
           </div>
@@ -174,7 +174,7 @@ export default function New() {
         {/* Textarea */}
         <div className={`${editorHidden ? "hidden" : "block"}`}>    
         <textarea ref={textareaRef} className="w-full resize-none p-4 bg-transparent text-brand-black focus:outline-none text-xl leading-snug placeholder-brand-grey-500 py-12 min-h-screen focus:border-gray-50 focus:bg-white focus:ring-0 border-gray-50"
-        spellCheck="false" style={{"height": "1471px !important;"}} value={markText} onChange={(e) => setMarkText(e.target.value)} placeholder="How was your day?..."/>
+        spellCheck="false" style={{"height": "1471px !important"}} value={markText} onChange={(e) => setMarkText(e.target.value)} placeholder="Paste the markdown..."/>
     </div>
     {/* Output */}
     <div ref={markdownRef} className={`max-w-screen-md mx-auto font-inter m-2 p-4 ${editorHidden ? "block" : "hidden"}`}>
