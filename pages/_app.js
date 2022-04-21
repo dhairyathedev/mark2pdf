a88edf73a92ce48e64c77d23970e8f76629cc8ad
SnowBit <dtshah24022006@gmail.com>
2022-04-21 11:06:44 +0530

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Script async src={`https://www.googletagmanager.com/gtag/js?id=G-RZCHR4SZ18`}></Script>
+    <Script id="google-analytics" strategy="lazyOnload">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RZCHR4SZ18', {
+                page_path: window.location.pathname,
+              });
+                  `}
+        </Script>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
