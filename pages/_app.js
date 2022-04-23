741e8d2a08e4498e310203348ea9f0da6d05a9b6
Dhairya Shah <dtshah24022006@gmail.com>
2022-04-23 14:15:06 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,13 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return(
     <>
-    <Script async src={`https://www.googletagmanager.com/gtag/js?id=G-RZCHR4SZ18`}></Script>
+    <Script async src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_GTAG}`}></Script>
     <Script id="google-analytics" strategy="lazyOnload">
           {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-RZCHR4SZ18', {
+              gtag('config', 'G-${process.env.NEXT_PUBLIC_GTAG}', {
                 page_path: window.location.pathname,
               });
                   `}
